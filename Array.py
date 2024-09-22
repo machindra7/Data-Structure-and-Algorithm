2ba5fd402ae81dad2aa63793c876bf03dd65b7e4
machindra7 <150156282+machindra7@users.noreply.github.com>
2024-09-22 23:51:16 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -21,12 +21,12 @@ my_array = array.array('i', [1, 2, 3, 4])
 print(my_array)
 
 # insert at beginning        # timecomplexity = O(N)   # spacecomplexity = O(1)
-# my_array.insert(0, 6)  
-# print(my_array)
+my_array.insert(0, 6)  
+print(my_array)
 
 # insert at middle 
-# my_array.insert(2, 5)
-# print(my_array) 
+my_array.insert(2, 5)
+print(my_array) 
 
 # insert at end
 my_array.insert(4, 6)
